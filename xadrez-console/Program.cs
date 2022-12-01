2abb6a1d137d6d479e6e030e398e70c47c1fc5ef
MiguelCrReis <miguelcr.reis@gmail.com>
2022-12-01 20:47:44 -0300

    Criado a classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using tabuleiro;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Posicao P = new Posicao(10, 10);
+
+        Console.WriteLine("Posição: " + P);
+        Console.ReadLine();
+
+    }
+}

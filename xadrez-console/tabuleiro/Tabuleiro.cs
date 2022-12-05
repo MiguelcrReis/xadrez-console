644f2f936e7d23b5571cb435b6c66776b5dd00c9
MiguelCrReis <miguelcr.reis@gmail.com>
2022-12-05 19:55:14 -0300

    Criado validação de posições. Funções para validar se a posição existe e se está ocupada já por alguma peça. Criado classe de Exception para o Tabuleiro.

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -18,10 +18,38 @@
             return pecas[linha, coluna];
         }
 
+        public Peca peca(Posicao posicao)
+        {
+            return pecas[posicao.linha, posicao.coluna];
+        }
+
+        public bool existePeca(Posicao posicao)
+        {
+            validaPosicao(posicao);
+            return peca(posicao) != null;
+        }
+
         public void colocarPeca(Peca peca, Posicao posicao)
         {
             pecas[posicao.linha, posicao.coluna] = peca;
-            peca.posicao = posicao; 
+            peca.posicao = posicao;
+        }
+
+        public bool posicaoValida(Posicao posicao)
+        {
+            if (posicao.linha < 0 || posicao.linha >= linhas || posicao.coluna < 0 || posicao.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void validaPosicao(Posicao posicao)
+        {
+            if (!posicaoValida(posicao))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 }

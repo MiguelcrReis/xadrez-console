461c8dff7d1a87dea493c61ce3fb8dca13595635
MiguelCrReis <miguelcr.reis@gmail.com>
2022-12-06 22:20:49 -0300

    Criação das peças faltantes.

---
+++ xadrez-console/xadrez/Bispo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez
+{
+    class Bispo : Peca
+    {
+        public Bispo(Tabuleiro tabuleiro, Cor cor) : base(tabuleiro, cor)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "B";
+        }
+    }
+}
---
+++ xadrez-console/xadrez/Dama.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez
+{
+     class Dama : Peca
+    {
+        public Dama(Tabuleiro tabuleiro, Cor cor) : base(tabuleiro, cor)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "D";
+        }
+    }
+}
